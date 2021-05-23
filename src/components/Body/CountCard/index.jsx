f2d7b47adcca2c15607aeb10b7f0bfcbea34eb3c
Olya <zaychenok37@gmail.com>
2021-05-23 20:07:00 +0300

    Add functionality in CountCard

--- src/components/Body/CountCard/index.jsx
+++ src/components/Body/CountCard/index.jsx
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const colors = ['red', 'blue', 'green', 'yellow', 'cyan', 'magenta', 'black', 'gray'];
 
 const CountCard = () => {
   const [count, setCount ]= useState(0);
+  const [color, setColor] = useState('red');
+  const [isRed, setIsRed] = useState(true);
 
-  const increment = () => setCount(count + 1);
+   
+
+  const increment = () => {
+    setCount(count + 1);
+    setIsRed(!isRed);
+    setColor(isRed ? color === colors.id : 'red');
+  };
  
   const decrement = () => setCount(count - 1);
+
+  
 
   return (
     <div>
       <h1>Card 1</h1>
       <h2>Count: {count}</h2>
-      <div>0</div>
+      <div style={{background:color}}>0</div>
       <button onClick={decrement}>minus</button>
       <button onClick={increment}>plus</button>
     </div>
